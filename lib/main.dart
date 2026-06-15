25d1660e7d678ba277a98986b7864abc43a2c977
Affiq <affiqfirdaus67@gmail.com>
2026-06-15 10:55:29 +0800

    notifications: added a notification feautre. but needs actual mobile phone to test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'app.dart';
+import 'services/notification_service.dart';
 
 // ============================================================
 // AllInOne App - Personal Management Mobile Application
@@ -15,6 +16,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await NotificationService.instance.initialize();
 
   runApp(const AllInOneApp());
 }
